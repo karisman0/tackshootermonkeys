59de0211f27afb23799b7fea791ec07e60990794
karisman0 <karolislipn@gmail.com>
2024-12-25 16:59:41 +0100

    change

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -9,5 +9,5 @@ public static class ModHelperData
     public const string Description = "Makes monkeys shoot like tack shooters";
 
     public const string RepoOwner = "karisman0"; // TODO add your github username hero, also in the download url in README.md
-    public const string RepoName = ""; // TODO add your repo name here, also in the download url in README.md
+    public const string RepoName = "tackshootermonkeys"; // TODO add your repo name here, also in the download url in README.md
 }
